1c415202f9c9146588bd8aa6bd7bd859bc743cf6
SarthakKuchhal <sarthak@credochain.com>
2020-08-30 03:44:23 +0530

    created frontpage

---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import LogoPic from "../Logo/face-id.svg"
+const Logo = () => {
+  return (
+    <div className="ma4 mt0">
+      <Tilt className="br2 shadow-2" style={{ height: "150px",width: "150px" }}>
+        <div className="pa3">
+          <img style={{ paddingTop: "5px" }} src={LogoPic} alt="logo"/>
+        </div>
+      </Tilt>   
+    </div>
+  );
+};
+
+export default Logo;
