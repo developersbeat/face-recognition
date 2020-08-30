8dc2f251a477a19f7ad38afe85ac45ded3d6db3f
SarthakKuchhal <sarthak@credochain.com>
2020-08-31 03:39:54 +0530

    added background particles

--- src/App.js
+++ src/App.js
@@ -2,14 +2,43 @@ import React, { Component } from "react";
 import "./App.css";
 import Navigation from "./Components/Navigation/Navigation";
 import Logo from "./Components/Logo/Logo";
+import Rank from "./Components/Rank/Rank";
 import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
+import { Particles } from "react-particles-js";
 
+const ParticlesOptions = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area:300 
+      }
+    },
+    size: {
+      value: 4,
+    },
+  },
+ 
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse",
+      },
+    },
+  },
+
+};
 export default class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={ParticlesOptions} />
+
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/* <FaceRecognation /> */}
       </div>
--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -3,14 +3,14 @@ import "./ImageLinkForm.css";
 const ImageLinkForm = () => {
     return (
         <div>
-            <p className="f3">
+            <p className="f3 fw7">
                 {"This Face Scanner will detect faces in your Pictures. Give it a Try"}
             </p>
             <div className="center">
                 <div className="form center pa4 b3 shadow-5">
                 <input type="text" className="f4 pa2 w-70 center"/>
-                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
-               
+                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple pointer">Detect</button>
+                    
                 </div>
             </div>
         </div>
